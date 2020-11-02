664c8f973cdd4ca1a8fb18307846449e112e5d26
Rodrigo Valle <rdan.valle@gmail.com>
2020-11-01 19:17:18 -0800

    Implement peer insertion and lookup

--- src/tracker.rs
+++ src/tracker.rs
@@ -2,14 +2,17 @@ use crate::Opt;
 
 use hyper::{Body, Method, Request};
 use serde::{de, ser, Deserialize, Serialize};
+use rand::seq::IteratorRandom;
 use serde_urlencoded;
+
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::convert::{TryFrom, TryInto};
 use std::fmt;
 use std::net::IpAddr;
 use std::str;
 use std::sync::Mutex;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 pub type TrackerResult = Result<TrackerResponse, TrackerError>;
 
@@ -32,29 +35,24 @@ impl TrackerError {
     }
 }
 
-#[derive(Debug, Serialize)]
+// Hash is used to avoid duplicates
+// Consider ignoring peer_id so that changing peer_id doesn't cause us to store duplicate ip/port
+// combinations in the hashset of a torrent.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize)]
 pub struct Peer {
     // peer's self selected ID
     #[serde(rename = "peer id")]
     peer_id: PeerId,
     // peer's Ipv4/6 address or DNS name
-    ip: PeerAddress,
+    ip: IpAddr,
     port: u16,
-}
-
-#[derive(Debug, Serialize)]
-#[serde(untagged)]
-enum PeerAddress {
-    Ip(IpAddr),
-    Dns(String),
 }
 
 // TODO: newtype can borrow from the deserializer as long as the deserializer is alive
 // TODO: consider serde_bytes?
 macro_rules! newtype_bytearray {
     ($newtype:ident, $len:expr) => {
-        //#[serde(try_from = "Vec<u8>")]
-        #[derive(Clone, Debug, Eq, PartialEq, Hash)]
+        #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
         struct $newtype([u8; $len]);
 
         // by default serde_bencode will serialize/deserialize byte arrays as bencoded lists of
@@ -84,6 +82,13 @@ macro_rules! newtype_bytearray {
 
                     fn visit_bytes<E: de::Error>(self, value: &[u8]) -> Result<Self::Value, E> {
                         match Self::Value::try_from(value) {
+                            Ok(ret) => Ok(ret),
+                            _ => Err(E::invalid_length(value.len(), &self)),
+                        }
+                    }
+
+                    fn visit_str<E: de::Error>(self, value: &str) -> Result<Self::Value, E> {
+                        match Self::Value::try_from(value.as_bytes()) {
                             Ok(ret) => Ok(ret),
                             _ => Err(E::invalid_length(value.len(), &self)),
                         }
@@ -137,6 +142,10 @@ impl TrackerRequest {
         // ret.map_err(|s: &str| TrackerError::new(s.to_string()))
         Ok(())
     }
+
+    fn normalize_request(&mut self) {
+        self.numwant = self.numwant.or(Some(50));
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -153,7 +162,8 @@ enum ClientEvent {
 pub struct Tracker {
     opt: Opt,
     // TODO: replace with a concurrent hashmap for finer grained locking?
-    torrents: Mutex<HashMap<InfoHash, Vec<Peer>>>,
+    torrents: Mutex<HashMap<InfoHash, HashSet<Peer>>>,
+    complete_count: AtomicU32,
 }
 
 impl Tracker {
@@ -161,36 +171,63 @@ impl Tracker {
         Self {
             opt,
             torrents: Mutex::new(HashMap::new()),
+            complete_count: AtomicU32::new(0),
         }
     }
 
-    fn register_new_peer(&self, req: &TrackerRequest) {
+    /// Registers a new peer as interested in a torrent if we don't already know about this peer.
+    fn maybe_register_new_peer(&self, req: &TrackerRequest) {
         let mut torrents = self.torrents.lock().unwrap();
         let peer = Peer {
             peer_id: req.peer_id.clone(), // could probably have this be a borrow?
-            ip: PeerAddress::Ip(req.ip.unwrap()), // TODO: this unwrap is bad
+            ip: req.ip.unwrap(), // TODO: we might need to infer the client's IP
             port: req.port,
         };
 
         torrents
-            .entry(req.info_hash.clone())
-            .or_insert(vec![])
-            .push(peer);
+            .entry(req.info_hash.clone()) // we identify a torrent by its info_hash
+            .or_insert(HashSet::new()) // create a mapping for new torrents
+            .insert(peer); // track all the peers participating in this torrent
+    }
+
+    /// Pick `numwant` number of random peers, excluding the client making this request, from the
+    /// torrent that the client is interested in.
+    // TODO: exclude the requester from the peer list
+    fn get_peers(&self, req: &TrackerRequest) -> Vec<Peer> {
+        let torrents = self.torrents.lock().unwrap();
+        let mut rng = rand::thread_rng();
+        let peers = torrents
+            .get(&req.info_hash)
+            .map_or(vec![], |peers: &HashSet<Peer>| {
+                // we can copy these out or return the MutexGuard
+                // since these borrow from the `torrents` MutexGuard we are not allowed to return
+                // references without also holding the lock.
+                peers.iter().choose_multiple(&mut rng, req.numwant.unwrap() as usize)
+            });
+
+        peers.into_iter().copied().collect()
     }
 
     pub fn handle_session(&self, req: Request<Body>) -> TrackerResult {
         let uri = req.uri();
         let ret = match (req.method(), uri.path(), uri.query()) {
             (&Method::GET, "/announce", Some(query)) => {
-                let qs = TrackerRequest::from_query_string(query)?;
+                let mut qs = TrackerRequest::from_query_string(query)?;
                 qs.validate_request()?;
+                qs.normalize_request();
+                self.maybe_register_new_peer(&qs);
                 match qs.event {
-                    Some(ClientEvent::Started) => self.register_new_peer(&qs),
+                    Some(ClientEvent::Started) => unimplemented!(),
                     Some(ClientEvent::Stopped) => unimplemented!(),
-                    Some(ClientEvent::Completed) => unimplemented!(),
-                    None => unimplemented!(),
+                    Some(ClientEvent::Completed) => {
+                        self.complete_count.fetch_add(1, Ordering::Relaxed);
+                    },
+                    None => {}
                 }
-                unimplemented!();
+                Ok(TrackerResponse {
+                    interval: 1,
+                    peers: self.get_peers(&qs),
+                })
             }
             (&Method::GET, "/announce", None) => Err("Invalid request: no query string."),
             (&Method::GET, _, _) => Err("Unrecognized path, try '/announce'."),
@@ -221,14 +258,24 @@ mod test {
 
     #[test]
     fn peer_id_de_test() {
+        #[derive(Debug, Deserialize, PartialEq)]
+        struct TestData {
+            peer_id: PeerId,
+        }
+
         let hash: [u8; 20] = ['a' as u8; 20];
-        let peer_id = PeerId(hash);
+        let test_data = TestData { peer_id: PeerId(hash) };
 
         // this is throwing an error because serde can't deserialize a byte array into `an array of
         // length 20`
         assert_eq!(
-            peer_id,
+            PeerId(hash),
             serde_bencode::from_str::<PeerId>("20:aaaaaaaaaaaaaaaaaaaa").unwrap(),
+        );
+
+        assert_eq!(
+            test_data,
+            serde_urlencoded::from_str::<TestData>("peer_id=aaaaaaaaaaaaaaaaaaaa").unwrap(),
         );
     }
 
@@ -241,7 +288,7 @@ mod test {
     fn basic_ok_test() {
         let peer = Peer {
             peer_id: PeerId("abcdefghijklmnopqrst".as_bytes().try_into().unwrap()),
-            ip: PeerAddress::Ip(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))),
+            ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
             port: 6981,
         };
         let response = TrackerResponse {
